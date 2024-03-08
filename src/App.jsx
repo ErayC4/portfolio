631b8e71b12e8fe00601efe39c737ba890d31e5e
ErayC4 <eray04.coskun@gmail.com>
2024-03-08 20:52:07 +0100

    title and placeholder image

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="text-white h-full flex items-center">
+    <div className="pl-12">
+        <div className="text-7xl pb-8">
+            <p>Reach for the stars</p>
+            <p>& Develop the future</p>
+        </div>
+        <button className="px-12 border border-white text-lg py-2 rounded-full">Learn More</button>
+    </div>
+    <img src="/images/jupiter.jpg" className='inset-0 mt-96 object-cover' alt="" />
+</div>
+
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,10 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <div className='bg-black h-screen'>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </div>
+
 )
