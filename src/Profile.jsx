ac59c8bd5091b2bd2aa711a44485740a09c73e40
ErayC4 <eray04.coskun@gmail.com>
2024-03-13 19:30:59 +0100

    added foundation of image carousel

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -4,7 +4,40 @@ function Profile() {
   return (
     <div className="h-screen">
       <div className="w-1/3">
+        
         <p className="text-8xl pb-8">Profile</p>
+        <div>
+          <div className="flex justify-between w-96 items-center text-lg border-2 border-white py-2 px-12">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-chevron-left"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"
+              />
+            </svg>
+            <p>ImageName</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-chevron-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"
+              />
+            </svg>
+          </div>
+          <img src="/images/affen-selfie-peta-david-slater.jpg" className="cover" alt="" />
+        </div>
         <p className="text-2xl pb-4">
           Hey, I am a self taught UI/UX designer and Web-Developer, with a
           passion to learn new skills and technologies.
